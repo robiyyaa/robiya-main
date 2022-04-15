e2526b2d79eeede833312a11f30c8f3584216527
klubni1chka <97652254+klubni1chka@users.noreply.github.com>
2022-04-15 23:33:17 +0600

    ыыыы

---
+++ test1.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function($) {
+  $("#scroll-top > button").on("click", function(e) {
+	var body = $("html, body");
+	body.stop().animate({ scrollTop: 0 }, 500, "swing");
+  });
+});
